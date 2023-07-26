0a8a2f7b06ee77e2f4f7a5f0655cbe8e2d844997
Azadeh Galidari <agalidari@gmail.com>
2023-07-26 13:45:54 +0200

    using component composition s a wrapper around othre component

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import "./ExpenceItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
 function ExpenseItem(props) {
     return (
-        <div className='expense-item'>
+        <Card className='expense-item'>
             {/* <div>{props.expense.date.toISOString()}</div> */}
             <ExpenseDate date={props.item.date} />
             <div className='expense-item__description'>
                 <h2>{props.item.title}</h2>
                 <div className='expense-item__price'>{props.item.amount}</div>
             </div>
-        </div>
+        </Card>
     );
 }
 
--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 
 function Expenses(props) {
     return (
-        <div className='expenses'>
+        <Card className='expenses'>
             <ExpenseItem item={props.items[0]} />
             <ExpenseItem item={props.items[1]} />
             <ExpenseItem item={props.items[2]} />
             <ExpenseItem item={props.items[3]} />
-        </div>
+        </Card>
     );
 }
 
