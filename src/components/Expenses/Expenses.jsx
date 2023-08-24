06d675720979a6036ffa43eda9504c9ac92d7ce2
Azadeh Galidari <agalidari@gmail.com>
2023-08-24 23:41:30 +0200

    added ExpensesFilter

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 
 const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = (selectedYear) => {
+        console.log(selectedYear);
+        setFilteredYear(selectedYear);
+        console.log('from state =>', filteredYear);
+    };
+
     return (
-        <Card className='expenses'>
-            <ExpenseItem item={props.items[0]} />
-            <ExpenseItem item={props.items[1]} />
-           {/*  <ExpenseItem 
+        <div>
+            <Card className='expenses'>
+                <ExpensesFilter onChangeFilter={filterChangeHandler} selected={filteredYear}/>
+                <ExpenseItem item={props.items[0]} />
+                <ExpenseItem item={props.items[1]} />
+                {/*  <ExpenseItem 
                 title={props.items[1].title}
                 amount={props.items[1].amount}
                 date={props.items[1].date}
             /> */}
-            <ExpenseItem item={props.items[2]} />
-            <ExpenseItem item={props.items[3]} />
-        </Card>
+                <ExpenseItem item={props.items[2]} />
+                <ExpenseItem item={props.items[3]} />
+            </Card>
+        </div>
     );
 };
 
